f6a2e8cb99d4c1444eb03f767550b61fd8f9d1c0
Cesar Muzio <cmuzio375@gmail.com>
2022-03-24 15:03:08 -0300

    cambio en el modelo de movies_characters

--- models/movies_characters.js
+++ models/movies_characters.js
@@ -7,6 +7,12 @@ const db = require('../db/config');
 
 const movies_characters = db.define('movies_characters', {
 
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
 
     PersonajeId:{
         type: DataTypes.INTEGER
