74efa8082de1c8db4b90ead33a6273e925dfd83b
Cesar Muzio <cmuzio375@gmail.com>
2022-03-24 12:33:39 -0300

    test6

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 
-//console.log( process.env.DB_NAME ,process.env.DB_USERNAME, process.env.DB_PASS, process.env.DB_HOST)
+console.log( process.env.DB_NAME ,process.env.DB_USERNAME, process.env.DB_PASS, process.env.DB_HOST)
 const db = new Sequelize(process.env.DB_NAME ,process.env.DB_USERNAME, process.env.DB_PASS,{
   host: process.env.DB_HOST,
   dialect: 'mysql',
