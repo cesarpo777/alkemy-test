eb4e5638babbae7a398fc4b8cc819a1d49e02e34
Cesar Muzio <cmuzio375@gmail.com>
2022-03-24 11:31:43 -0300

    testing 1

--- db/config.js
+++ db/config.js
@@ -1,13 +1,25 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('alkemy', 'root', 'cesar',{
+
+
+const db = new Sequelize(process.env.DB_NAME ,process.env.DB_USERNAME, process.env.DB_PASS,{
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  dialectOptions: {
+      multipleStatements: true
+    }
+  //logging: false
+})
+
+// LOCAL ENVIROMENT
+/* const db = new Sequelize('alkemy', 'root', 'cesar',{
     host:'localhost',
     dialect: 'mysql',
     dialectOptions: {
         multipleStatements: true
       }
     //logging: false
-}) 
+}) */
 
     
 
